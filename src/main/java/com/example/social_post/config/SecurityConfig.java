9588b587aca347f74d697071e556cce682e21836
deepakSingh023 <dsinghrana71@gmail.com>
2026-01-02 18:09:50 +0530

    security config

--- src/main/java/com/example/social_post/config/SecurityConfig.java
+++ src/main/java/com/example/social_post/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000")); // ✅ allows all domains
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000","https://post-social-dwlc.onrender.com")); // ✅ allows all domains
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
